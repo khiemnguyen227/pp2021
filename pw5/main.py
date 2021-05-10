c374148dfd17e4da6e7dd8b0927a05b80f14fe8a
killeno227 <killeno227@yahoo.com>
2021-05-10 13:49:10 +0700

    fix pw5

--- pw5/main.py
+++ pw5/main.py
@@ -1,4 +1,6 @@
 import curses
+import os
+import zipfile
 from output import OutputModule
 from input import InputModule
 from domains.Student import *
@@ -33,3 +35,18 @@ class MainModule:
 
 mark_gpa()
 mark_cal()
+
+pw5 = zipfile.ZipFile('D:\\Studies\\B2 subjects\\git\\pp2021\\pw5\\students.dat', "w")
+
+for folder, subfolders, files in os.walk('D:\\Studies\\B2 subjects\\git\\pp2021\\pw5'):
+
+    for file in files:
+        if file.endswith('.py'):
+            pw5.write(os.path.join(folder, file),
+                      os.path.relpath(os.path.join(folder, file), 'D:\\Studies\\B2 subjects\\git\\pp2021\\pw5\\'),
+                      compress_type=zipfile.ZIP_DEFLATED)
+
+pw5 = zipfile.ZipFile('D:\\Studies\\B2 subjects\\git\\pp2021\\pw5\\students.dat')
+pw5.extractall('D:\\Studies\\B2 subjects\\git\\pp2021\\pw5\\Compress and Decompress\\')
+
+pw5.close()
